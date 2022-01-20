dd811b95092ceb378a2d8dace2721421e9247dfe
Garth Verdeflor <garthverdeflor@gmail.com>
2022-01-20 17:33:53 -0500

    Set up and initialized Module 3

---
+++ module3/module3/module3.runs/synth_1/.Xil/module3_hw_wrapper_propImpl.xdc
@@ -0,0 +1,3 @@
+set_property SRC_FILE_INFO {cfile:/thayerfs/home/f00436n/Xilinx/Zybo-Z7-Master.xdc rfile:../../../../../../Xilinx/Zybo-Z7-Master.xdc id:1} [current_design]
+set_property src_info {type:XDC file:1 line:148 export:INPUT save:INPUT read:READ} [current_design]
+set_property -dict { PACKAGE_PIN V15   IOSTANDARD LVCMOS33     } [get_ports { pwm0_0 }]; #IO_L10P_T1_34 Sch=jc_p[1]
---
+++ module3/module3_sw_system/_ide/scripts/debugger_module3_sw-default.tcl
@@ -0,0 +1,29 @@
+# Usage with Vitis IDE:
+# In Vitis IDE create a Single Application Debug launch configuration,
+# change the debug type to 'Attach to running target' and provide this 
+# tcl script in 'Execute Script' option.
+# Path of this script: /thayerfs/home/f00436n/engs62/module3/module3_sw_system/_ide/scripts/debugger_module3_sw-default.tcl
+# 
+# 
+# Usage with xsct:
+# To debug using xsct, launch xsct and run below command
+# source /thayerfs/home/f00436n/engs62/module3/module3_sw_system/_ide/scripts/debugger_module3_sw-default.tcl
+# 
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~"APU*"}
+rst -system
+after 3000
+targets -set -filter {jtag_cable_name =~ "Digilent Zybo Z7 210351A6C79EA" && level==0 && jtag_device_ctx=="jsn-Zybo Z7-210351A6C79EA-13722093-0"}
+fpga -file /thayerfs/home/f00436n/engs62/module3/module3_sw/_ide/bitstream/module3_hw_wrapper.bit
+targets -set -nocase -filter {name =~"APU*"}
+loadhw -hw /thayerfs/home/f00436n/engs62/module3/module3_hw_wrapper/export/module3_hw_wrapper/hw/module3_hw_wrapper.xsa -mem-ranges [list {0x40000000 0xbfffffff}] -regs
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*"}
+source /thayerfs/home/f00436n/engs62/module3/module3_sw/_ide/psinit/ps7_init.tcl
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "*A9*#0"}
+dow /thayerfs/home/f00436n/engs62/module3/module3_sw/Debug/module3_sw.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "*A9*#0"}
+con
